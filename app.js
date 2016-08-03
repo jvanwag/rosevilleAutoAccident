13c662f29c23946a721ec462f5e89986662b7686
Jon Van Wagenen <jvanwag08@gmail.com>
2016-08-03 13:55:45 -0600

    changed colors and added in routes

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
 angular.module('accidentApp', ['ui.router'])
+.config(function($stateProvider, $urlRouterProvider, $httpProvider) {
+
+  $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+    .state('home', {
+      url: '/',
+      templateUrl: 'views/homeTemp.html',
+      controller: 'mainCtrl',
+    })
+    .state('', {
+      url: '/'
+    })
+})
